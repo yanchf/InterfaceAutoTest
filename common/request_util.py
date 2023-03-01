6e49722ba14aa6be498ef83db4a297b0ac6c6096
yanchf <1847572557@qq.com>
2023-03-01 21:12:57 +0800

    小优化

--- common/request_util.py
+++ common/request_util.py
@@ -38,15 +38,18 @@ class RequestUtil:
                 params_data = data["params"]
 
                 self.logging_util.info(url)
-                self.logging_util.info(json_data)
+                self.logging_util.info(data["json_data"])
+                self.logging_util.info(data["params"])
 
                 responses = self.session.request(method=data["method"], url=url, json=json_data, params=params_data, **self.kwargs)
             elif data["x_www_form_urlencoded_data"] is not None:
                 url = data["api"]
                 data_data = json.loads(data["x_www_form_urlencoded_data"])
                 params_data = data["params"]
+
                 self.logging_util.info(url)
-                self.logging_util.info(data_data)
+                self.logging_util.info(data["x_www_form_urlencoded_data"])
+                self.logging_util.info(data["params"])
 
                 responses = self.session.request(method=data["method"], url=url, data=data_data, params=params_data, **self.kwargs)
             else:
--- readme.md
+++ readme.md
@@ -14,7 +14,8 @@ B.使用方法
     3.更换pip源
     4.再根目录下打开cmd，输入pip install -r requirements.txt。初始化项目环境
     5.在testcase_data/excel/testcase_api.xlsx 中编写接口相关测试数据
-    6.在cmd界面中输入run_test.py 或者 python run_test.py 运行项目
+    6.config.py中配置钉钉机器人相关
+    7.在cmd界面中输入run_test.py 或者 python run_test.py 运行项目
     
 
 C.*要点须知
